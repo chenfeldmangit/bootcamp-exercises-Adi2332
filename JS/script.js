e1e9f626673157bcd58f950cc84a6b38a058de5b
Adi Magen <adi.magen@gong.io>
2020-03-31 14:09:35 +0300

    read text with new line

--- JS/script.js
+++ JS/script.js
@@ -13,7 +13,7 @@ class Profile {
 class Tweet {
     constructor(text, like = false) {
         this.id = getNewTweetId();
-        this.text = text;
+        this.text = text.replace(/\r?\n/g, '<br />');;
         this.like = like;
     }
 }
